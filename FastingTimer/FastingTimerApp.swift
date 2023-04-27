6e5b9ba4b40bc53bf480306e34a2a57e73211100
Grace, Mu-Hui Yu <muhuiyu.grace@gmail.com>
2023-04-27 19:30:14 +0800

    first commit

---
+++ FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Mu Yu on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
